818aed18d3632e44f2ccec714e37f2f510e027f0
Jamie Pinheiro <jamiepinheiro@gmail.com>
2015-10-31 23:09:54 -0400

    init

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,27 @@
+class PicsController < ApplicationController
+  def index
+  	@pics = Pic.all
+  end
+  def show 
+  	@pic = Pic.find(params[:id])
+  end
+
+  def new
+    @pic = Pic.new
+  end
+  
+  def create
+    @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to '/pics'
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def pic_params
+    params.require(:pic).permit(:title, :image)
+  end
+
+end
