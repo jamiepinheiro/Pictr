3a1a591cd632f2dc3162c69f8909b0e46b13a6d7
Jamie Pinheiro <jamiepinheiro@gmail.com>
2015-11-05 19:25:25 -0500

    visual

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101012208) do
+ActiveRecord::Schema.define(version: 20151105003411) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pics", force: :cascade do |t|
     t.string   "image"
